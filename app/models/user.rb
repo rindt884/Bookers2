4ef428b9deb9563f633fc90bf9994c57822f238b
EC2 Default User <ec2-user@ip-172-31-35-136.us-west-2.compute.internal>
2022-06-27 11:41:08 +0000

    Bootstrap added

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
   validates :introduction, presence: false, length: { maximum: 50 }
-  validates :name, presence: true, length: { in: 2..20 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 20}, uniqueness: true
   
 
   def get_profile_image(width, height)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
-  get "/homes/about" => "homes#about", as: "about"
-  
+  get "/home/about" => "homes#about"
+
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :index, :update]
 
